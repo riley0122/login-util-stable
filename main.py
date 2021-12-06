0930fb10c1d3ea85455ef28f69a6706b3d3a04d0
lucasammer <77288737+lucasammer@users.noreply.github.com>
2021-12-06 11:34:37 +0000

    added main login fucnction

--- main.py
+++ main.py
@@ -3,5 +3,5 @@ import os
 os.system('pip install --upgrade pip')
 os.system('pip install wheel')
 os.system('pip install twine')
-os.system('python example_project/setup.py sdist bdist_wheel')
+os.system('setup/setup.py sdist bdist_wheel')
 os.system('twine upload dist/*')
--- setup/setup.py
+++ setup/setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fhandle:
     long_description = fhandle.read() # Your README.md file will be used as the long description!
 
 setuptools.setup(
-    name="lucasammer", # Put your username here!
+    name="login-util", # Put your username here!
     version="0.0.1", # The version of your package!
     author="lucasammer", # Your name here!
     author_email="me@lucasammer.com", # Your e-mail here!
     description="login util", # A short description here!
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject", # Link your package website here! (most commonly a GitHub repo)
+    url="https://github.com/lucasammer/login-util-stable", # Link your package website here! (most commonly a GitHub repo)
     packages=setuptools.find_packages(), # A list of all packages for Python to distribute!
     classifiers=[
         "Programming Language :: Python :: 3",
