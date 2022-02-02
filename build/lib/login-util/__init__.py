178ae67589b864d11ed9d7c419b1639d317be42c
lucasammer <lucasammerlaan123@gmail.com>
2022-02-02 14:32:35 +0100

    trying to make it work

--- build/lib/login-util/__init__.py
+++ build/lib/login-util/__init__.py
@@ -1,1 +1,9 @@
-import loginUtil.py
+def login(usernameArray, passwordArray, inputUsername, Inputpassword):
+  if usernameArray.includes(inputUsername):
+    uan = usernameArray.index(inputUsername)
+    if passwordArray.includes(Inputpassword):
+      pan = passwordArray.index(Inputpassword)
+      if pan == uan: 
+        return True
+    else: return False
+  else: return False
